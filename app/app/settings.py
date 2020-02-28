21a0264c178010f9551ccb2b93d2095a76efee39
Alejandro Mosso <alejandro.mosso@smsoftware.com.mc>
2020-02-28 15:53:16 -0600

    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')

--- app/app/settings.py
+++ app/app/settings.py
@@ -122,4 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+# Line added
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Line added
+
 STATIC_URL = '/static/'
