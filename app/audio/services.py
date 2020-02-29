1dbe9a13ee077140e0eddf5b39fee7ed5093e62d
Alejandro Mosso <alejandro.mosso@smsoftware.com.mc>
2020-02-29 13:24:38 -0600

    Procfile with unicorn config

--- app/audio/services.py
+++ app/audio/services.py
@@ -7,28 +7,18 @@ class AudioService:
     url_english = 'https://dictionary.cambridge.org/pronunciation/english/'
 
     def getAudio(self, word, language):
-        print('Start service')
-        array: any
         response: requests.Response
+        clean_a = []
         if language == 'english':
             response = requests.get(url=self.url_english + word)
 
-        clean_a = []
-        array = response.text.split('\n')
-        print('Got response ', getsizeof(array))
-        """
-        print(array.nbytes)
-        index = np.char.find(array, 'itemprop="contentURL"')
-        i = np.where(index != -1)
-        index = np.char.find(array[i], '.mp3')
-        i2 = np.where(index != -1)
+            array = response.text.split('\n')
 
-        if len(array[i][i2]) == 0:
-            return {}
+            for a in array:
+                if 'itemprop="contentURL"' in a:
+                    clean_a.append(a.replace('<meta itemprop="contentURL" content="', '').replace('"', '').\
+                        replace('/>', '').strip())
+
+            return {'audios': clean_a}
         else:
-            array = np.unique(array[i][i2])
-            for x in array:
-                clean_a.append(x.replace('<meta itemprop="contentURL" content="', '').replace('"', '').\
-                    replace('/>', '').strip())
-        """
-        return {'audios': clean_a}
+            return {'audios': []}
